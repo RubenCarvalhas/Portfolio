f4b39575a1b6ca173099fecb56a2d752c8572d1f
RubenCarvalhas <130073685+RubenCarvalhas@users.noreply.github.com>
2025-09-18 17:39:57 +0100

    First Commit

---
+++ js/app.js
@@ -0,0 +1,77 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 1.5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.5,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+      
+      }
+    },
+
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
---
+++ js/mainjs.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const titles = document.querySelectorAll('.secondTitle');
+
+  const observer = new IntersectionObserver((entries, obs) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('secondTitle--visible');
+        obs.unobserve(entry.target); // deixa de observar depois da animação
+      }
+    });
+  }, { threshold: 0.9 }); // 10% visível já conta
+
+  titles.forEach(title => observer.observe(title));
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const lines = document.querySelectorAll('.aline, .secondLine');
+
+  const observerLines = new IntersectionObserver((entries, obs) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible'); // ativa a animação
+        obs.unobserve(entry.target); // só anima 1 vez
+      }
+    });
+  }, { threshold: 0.5 }); // 50% visível já dispara
+
+  lines.forEach(line => observerLines.observe(line));
+});
